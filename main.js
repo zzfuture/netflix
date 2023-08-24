43eff1bb44128a2e4a880b26ff3920d369270203
zzfuture <113058239+zzfuture@users.noreply.github.com>
2023-08-24 13:09:43 -0500

    navbar js json

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
 let construirEncabezado = async(file)=>{
     let peticion = await fetch(`${file}.json`);
-    console.log(peticion)
     let res = await peticion.json();
-    console.log(res.section.parrafo[0].texto)
     let seleccion = document.querySelector("#encabezado");
     seleccion.insertAdjacentHTML("beforeend", /*html*/`
         <div class="pricing-header py-3 pb-md text-start">
-        <h1 class="fs-6 fw-normal text-body-emphasis">STEP 1 OF 3</h1>
-        <p class="fs-2 fw-bold">Choose the plan that’s right for you</p>
-        ${res.section.parrafo.map(value => /*html*/
+        <h1 class="fs-6 fw-normal text-body-emphasis">${res.header.steps}</h1>
+        <p class="fs-2 fw-bold">${res.header.titulo}</p>
+        ${res.header.parrafo.map(value => /*html*/
             `<p class="d-flex align-items-center fw-semibold"><svg class="bi text-danger" width="24" height="24">
                 <use xlink:href="#check" /></svg>&ensp;${value.texto}
             </p>`
         ).join('')}
-        </div>        
+        </div>
         `);
 }
-construirEncabezado("config")
+construirEncabezado("config");
+
+let construirNavbar = async(file)=> {
+    let peticion = await fetch(`${file}.json`);
+    let res = await peticion.json();
+    let seleccion = document.querySelector("#navbar");
+    seleccion.insertAdjacentHTML("beforeend", /*html*/`
+    <div class="container-fluid py-3">
+      <a class="px-5" href="/" class="d-flex align-items-center link-body-emphasis text-decoration-none">
+        <img class="netflixlogo"
+          src=${res.navbar.logo}
+          alt="logo">
+      </a>
+      <a class="px-5 link-body-emphasis text-decoration-none" href="#">Sign In</a>
+    </div>
+    `);
+}
+construirNavbar("config");
